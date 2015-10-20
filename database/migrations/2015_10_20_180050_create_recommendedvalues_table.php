d0acfea250412b936ba84724f43219dddf30f3a1
las203 <las203@case.edu>
2015-10-20 16:12:07 -0400

    Added new basic database structure

---
+++ database/migrations/2015_10_20_180050_create_recommendedvalues_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecommendedvaluesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recommendedvalues', function (Blueprint $table) {
+            $table->increments('id')->unsigned()->index();
+            $table->foreign('nutrient_name')->references('name')->on('nutrients');
+            $table->integer('age_range')->references('id')->on('ageranges');
+            $table->string('sex');
+            $table->float('daily_value');
+            $table->float('upper_limit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recommendedvalues');
+    }
+}
