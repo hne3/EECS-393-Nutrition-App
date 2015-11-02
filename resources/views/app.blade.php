117f86461f7913ee313f88ddde506a43013fc254
Eihiko <oeihiko@gmail.com>
2015-11-02 12:23:38 -0500

    Begin work on FAQ

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
     <title>Laravel</title>
 
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -35,6 +36,7 @@
             @if(Auth::check())
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/') }}">Home</a></li>
+                    <li><a href="{{ url('/faq') }}">FAQ</a></li>
                 </ul>
             @endif
             <ul class="nav navbar-nav navbar-right">
---
+++ resources/views/faq.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h2>FAQ</h2>
+            <p class="faq"><span class="faq">What is Snackr?</span> Snakr is an app.</p>
+            <p class="faq"><span class="faq">Why should I use Snackr?</span> Because I said so.</p>
+        </div>
+    </div>
+@endsection
