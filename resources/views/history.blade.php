3f513a57a0a9b77736139bd3e215527b43a84f6d
Lucy Tan <lucyktan@gmail.com>
2015-11-03 18:00:53 -0500

    Create food selection and history UI

---
+++ resources/views/history.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h2>Your Food History</h2>
+
+        </div>
+    </div>
+@endsection
