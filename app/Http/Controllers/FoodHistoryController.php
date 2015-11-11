d658af42fc425d2e26049c349e134a993cfaa93a
James Wright <jow5@case.edu>
2015-11-11 01:14:47 -0500

    History calculator updated to compute the past N days of nutrient totals along with all the other data.

--- app/Http/Controllers/FoodHistoryController.php
+++ app/Http/Controllers/FoodHistoryController.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Routing\Redirector;
@@ -12,62 +13,83 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Route;
 use App\Nutrient;
 
-class FoodHistoryController extends Controller 
+class FoodHistoryController extends Controller
 {
-	public function addFood()
-	{
-		if(Auth::check()){
-			$foodid = Request::get('foodid');
-			$food = Food::find($foodid);
-			if($food == null){
-				return 'Unprocessable Food.';
-			}
-			$quantity = Request::get('quantity');
-			if($quantity == null || $quantity == 0){
-				return 'Invalid Quantity.';
-			}
-			$user = Auth::user();
-			$user->addToFoodHistory($food,$quantity);
-			return Redirect::route('foodhistory');
-		} else {
-			return 'Please log in!';
-		}
-	}
+    public function addFood()
+    {
+        if (Auth::check()) {
+            $foodid = Request::get('foodid');
+            $food = Food::find($foodid);
+            if ($food == null) {
+                return 'Unprocessable Food.';
+            }
+            $quantity = Request::get('quantity');
+            if ($quantity == null || $quantity == 0) {
+                return 'Invalid Quantity.';
+            }
+            $user = Auth::user();
+            $user->addToFoodHistory($food, $quantity);
+            return Redirect::route('foodhistory');
+        } else {
+            return 'Please log in!';
+        }
+    }
 
-	public function index(){
-		$user = Auth::user();
-		$foods = $user->getFoodHistory();	
-			$totalCalories = 0;
-			$data = [];
-			$total = [];
+    public function index()
+    {
+        $user = Auth::user();
+        $foods = $user->getFoodHistory();
+        $todayTotalCalories = 0;
+        $todayData = [];
+        $todayNutrientTotals = [];
 
-			$nutrients = Nutrient::orderBy('name','ASC')->get();
-			foreach($nutrients as $nutrient){
-				$total[$nutrient->id] = 0;
-			}
-			foreach($foods as $food){
+        $numDays = 5;
 
-				$actualCalories = ($food->pivot->quantity / 100) * $food->getCalories();
-				$food->actualCalories = $actualCalories;
-				$totalCalories += $actualCalories;
+        //Index these arrays by $dayNumber - 1 (so that 1 day ago is in index 0), $nutrient->id
+        $previousTotalCalories = [];
+        $previousNutrientTotals = [];
 
-				$allNutrients = $food->nutrients;
-				$foodid = $food->id;
-				$quantity = $food->pivot->quantity;
-				foreach($allNutrients as $nutrient){
-					$data[$foodid][$nutrient->id] = $nutrient->pivot->amount_in_food * ($quantity)/100;
-					if(!array_key_exists($nutrient->id,$total)){
-						$total[$nutrient->id] = $data[$foodid][$nutrient->id];
-					} else {
-						$total[$nutrient->id] = $total[$nutrient->id] + $data[$foodid][$nutrient->id];
-					}
-				}
-				$otherNutrients = Nutrient::whereNotIn('id',$food->nutrients()->lists('nutrient_id')->toArray())->get();
-				foreach($otherNutrients as $nutrient){
-					$data[$foodid][$nutrient->id] = 0;
-					$total[$foodid][$nutrient->id] = 0;
-				}
-			}
-		return view('history')->with(compact('foods', 'dates', 'totalCalories', 'data', 'total', 'nutrients'));
-	}
+        $nutrients = Nutrient::orderBy('name', 'ASC')->get();
+        foreach ($nutrients as $nutrient) {
+            $todayNutrientTotals[$nutrient->id] = 0;
+            $previousNutrientTotals[$nutrient->id] = array_pad([], $numDays, 0);
+            $previousTotalCalories = array_pad([],$numDays,0);
+        }
+        foreach ($foods as $food) {
+            $daysSinceEaten = Carbon::Parse($food->pivot->timestamp)->diffInDays();
+            $actualCalories = ($food->pivot->quantity / 100) * $food->getCalories();
+            $food->actualCalories = $actualCalories;
+            if ($daysSinceEaten == 0) {
+                //We ate this today - it goes in a separate thing
+                $todayTotalCalories += $actualCalories;
+                $allNutrients = $food->nutrients;
+                $foodid = $food->id;
+                $quantity = $food->pivot->quantity;
+                foreach ($allNutrients as $nutrient) {
+                    $todayData[$foodid][$nutrient->id] = $nutrient->pivot->amount_in_food * ($quantity) / 100;
+                    if (!array_key_exists($nutrient->id, $todayNutrientTotals)) {
+                        $todayNutrientTotals[$nutrient->id] = $todayData[$foodid][$nutrient->id];
+                    } else {
+                        $todayNutrientTotals[$nutrient->id] = $todayNutrientTotals[$nutrient->id] + $todayData[$foodid][$nutrient->id];
+                    }
+                }
+                $otherNutrients = Nutrient::whereNotIn('id', $food->nutrients()->lists('nutrient_id')->toArray())->get();
+                foreach ($otherNutrients as $nutrient) {
+                    $todayData[$foodid][$nutrient->id] = 0;
+                }
+            } else if ($daysSinceEaten <= $numDays) {
+                //We want this many days worth of food data aggregated
+                $previousTotalCalories[$daysSinceEaten - 1] += $actualCalories;
+                $allNutrients = $food->nutrients;
+                $quantity = $food->pivot->quantity;
+                foreach ($allNutrients as $nutrient) {
+                    $actualNutrientValue = $nutrient->pivot->amount_in_food * ($quantity) / 100;
+                    $previousNutrientTotals[$nutrient->id][$daysSinceEaten-1] += $actualNutrientValue;
+                }
+            } else {
+                //Ignore this. Ideally, we would filter the query to only return items that fall in the previous two categories.
+            }
+        }
+        return view('history')->with(compact('foods', 'dates', 'totalCalories', 'data', 'total', 'nutrients'));
+    }
 }
