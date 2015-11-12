743e8ae82f860bdb70b93403bd5bf6f0ab530f87
Lucy Tan <lucyktan@gmail.com>
2015-11-12 13:42:08 -0500

    Shows nutrients for suggestion and gets another suggestion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -53,6 +53,8 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                            aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/suggestion') }}">Food Suggestion</a></li>
+                            <li><a href="{{ url('/food') }}">Food Search</a></li>
                             <li><a href="{{ url('/history') }}">Food History</a></li>
                             <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
                         </ul>
