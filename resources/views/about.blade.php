4d9e33ffbf89f2aa03d4e062ad51e13fba8d1610
las203 <las203@case.edu>
2015-11-13 11:26:10 -0500

    Changed the About Us page to have a legitimate message.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="jumbotron">
             <h2>About Us</h2>
-            <p>Blip. Bloop.</p>
+            <p>We are a group of college students at Case Western Reserve University taking EECS393.</p>
         </div>
     </div>
 @endsection
