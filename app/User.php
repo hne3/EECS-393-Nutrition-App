9a8fc282f49b06e050b68510b5df86f2ff1ff339
Lucy Tan <lucyktan@gmail.com>
2015-11-20 02:02:56 -0500

    SQL Query dump

--- app/User.php
+++ app/User.php
@@ -12,7 +12,6 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use App\Food;
 
-
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
@@ -83,7 +82,62 @@ class User extends Model implements AuthenticatableContract,
     }
 
     public function getFoodSuggestion(){
-        $suggestion = Food::orderByRaw("RAND()")->first();
-        return $suggestion;
+
+$score = DB::raw('
+SELECT 
+    foods.id as food_id, foods.name, SUM(fn.amount_in_food / rem_nutr.remaining_val) / (2000 / foods.calories) as score,
+    SUM(fn.amount_in_food / rem_nutr.remaining_val) as num, (2000 / foods.calories) as den
+FROM
+    foods
+        INNER JOIN
+    food_nutrient AS fn ON foods.id = fn.food_id
+        INNER JOIN   
+    (SELECT 
+        users.id,
+            fn.nutrient_id,
+            nutr.daily_value - SUM((quantity * amount_in_food / 100)) AS remaining_val
+    FROM
+        users
+            INNER JOIN 
+        user_history AS uh ON users.id = uh.user_id
+            INNER JOIN 
+        food_nutrient AS fn ON fn.food_id = uh.food_id
+            INNER JOIN 
+        (SELECT nutrient_id, daily_value
+         FROM recommended_values
+         WHERE age_range = 2 AND sex = 'F') AS nutr ON nutr.nutrient_id = fn.nutrient_id
+    WHERE
+        timestamp > DATE_SUB(NOW(), INTERVAL 24 HOUR)
+            AND 
+        users.id = 1
+    GROUP BY 
+        nutrient_id) AS rem_nutr ON rem_nutr.nutrient_id = fn.nutrient_id 
+GROUP BY foods.id order by score DESC, foods.id, fn.nutrient_id;')
+
+        // $recommended = \DB::table('recommended_values')
+        //         ->select('nutrient_id', 'daily_value')
+        //         ->where('age_range', '=', 2)        //get user's age range
+        //         ->where('sex', '=', 'F');            //get user's gender
+
+        // $user_rec = \DB::table('users')
+        //         ->join('user_history as uh', 'users.id', '=', 'uh.user_id')
+        //         ->join('food_nutrient as fn', 'fn.food_id', '=', 'uh.food_id')
+        //         ->join($recommended->toSql().' as nutr', 'nutr.nutrient_id', '=', 'fn.nutrient_id')
+        //         ->select('users.id', 'fn.nutrient_id', 
+        //             'nutr.daily_value - SUM((quantity * amount_in_food / 100)) as remaining_val')
+        //         ->where('timestamp', '>', 'strtotime(date(Y-m-d) -1 day') 
+        //             ->where('users.id', '=', 1)
+        //             ->groupBy('nutrient_id');
+
+        // $score = \DB::table('foods')
+        //             ->join('food_nutrient as fn', 'foods.id', '=', 'fn.food_id')
+        //             ->join($user_rec->toSql().' as rem_nutr', 'rem_nutr.nutrient_id', '=', 'fn.nutrient_id')
+        //             ->select('foods.id as food_id', 'foods.name', 
+        //                 DB::raw('SUM(fn.amount_in_food/rem_nutr.remaining_val)/(2000/foods.calories) as score'))
+        //             ->groupBy('foods.id')
+        //             ->groupBy('fn.nutrient_id')
+        //             ->orderBy('score', 'desc');
+
+        return $score;
     }
 }
